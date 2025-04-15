7cd35ecfdd203291ee33b4e7c87162cefd5258d7
Franchesca James <james.franchesca07@gmail.com>
2025-04-15 17:36:15 -0400

    add updateRoomDetails

---
+++ App/controllers/room.py
@@ -0,0 +1,13 @@
+from App.models import Room
+from App.database import db
+
+def updateRoomDetails(room_id, building_details_ID, name, floor, type):
+    room = Room.query.filter_by(id=room_id, building_id=building_details_ID).first()
+
+    if room:
+        rooms.name = name
+        rooms.floor = floor
+        rooms.type = type
+        db.session.commit()
+        return True
+    return False
