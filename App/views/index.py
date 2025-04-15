847e9c6e5d74e09707bc1656fae651d4e1bb7f03
Ishmael Jupiter <jupiterishmael@gmail.com>
2025-04-14 21:22:18 -0400

    Edited views

--- App/views/index.py
+++ App/views/index.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
 from App.controllers import create_user, initialize
-#from App.models import Location, BuildingDetails
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
 
@@ -16,13 +15,3 @@ def init():
 @index_views.route('/health', methods=['GET'])
 def health_check():
     return jsonify({'status':'healthy'})
-
-#@index_views.route('/location/<int:id>', methods=['GET'])
-#def location_detail(id):
-#    location = Location.query.get_or_404(id)
-#    return render_template('location_detail.html', location=location, is_authenticated=True)
-
-#@index_views.route('/building/<int:id>', methods=['GET'])
-#def building_detail(id):
-#    building = BuildingDetails.query.get_or_404(id)
- #   return render_template('building_detail.html', building=building, is_authenticated=True)
