8484773235e9e713a86ed7f1b00281faf4049194
Ishmael Jupiter <jupiterishmael@gmail.com>
2025-04-14 20:50:01 -0400

    Added necessary routes to views

--- App/views/admin.py
+++ App/views/admin.py
@@ -1,8 +1,8 @@
 from flask_admin.contrib.sqla import ModelView
 from flask_jwt_extended import jwt_required, current_user, unset_jwt_cookies, set_access_cookies
 from flask_admin import Admin
-from App.models import db, User
-
+from flask import render_template, flash, redirect, request, url_for  
+from App.models import db, User, Location, BuildingDetails, Room  
 class AdminView(ModelView):
 
     @jwt_required()
@@ -17,3 +17,24 @@ class AdminView(ModelView):
 def setup_admin(app):
     admin = Admin(app, name='FlaskMVC', template_mode='bootstrap3')
     admin.add_view(AdminView(User, db.session))
+
+from flask import Blueprint
+admin_views = Blueprint('admin_views', __name__, template_folder='../templates')
+
+@admin_views.route('/admin/locations')
+@jwt_required()
+def admin_locations():
+    locations = Location.query.all()
+    return render_template('admin/manage_locations.html', locations=locations, is_authenticated=True)
+
+@admin_views.route('/admin/buildings')
+@jwt_required()
+def admin_buildings():
+    buildings = BuildingDetails.query.all()
+    return render_template('admin/manage_buildings.html', buildings=buildings, is_authenticated=True)
+
+@admin_views.route('/admin/rooms')
+@jwt_required()
+def admin_rooms():
+    rooms = Room.query.all()
+    return render_template('admin/manage_rooms.html', rooms=rooms, is_authenticated=True)
