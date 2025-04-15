5bd048b5a88fb6683c064bea69e90e931bc077a9
Franchesca James <james.franchesca07@gmail.com>
2025-04-15 17:37:18 -0400

    add deleteRoom

--- App/controllers/room.py
+++ App/controllers/room.py
@@ -11,3 +11,12 @@ def updateRoomDetails(room_id, building_details_ID, name, floor, type):
         db.session.commit()
         return True
     return False
+
+    def deleteRoom(room_id, building_details_ID):
+    room = Room.query.filter_by(id=room_id, building_id=building_details_ID).first()
+    
+    if room:
+        db.session.delete(room)
+        db.session.commit()
+        return True
+    return False
