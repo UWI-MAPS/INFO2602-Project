2828013f2ba3a434ac3ed8435b679750942ff674
Franchesca James <james.franchesca07@gmail.com>
2025-04-15 20:00:47 -0400

    add updateLocationDetails

--- App/controllers/location.py
+++ App/controllers/location.py
@@ -11,3 +11,16 @@ def getLocation(location_id):
             print(f'{key}: {value}')
         return data
     return None
+
+def updateLocationDetails(location_id, name, image, latitude, longitude, type):
+    location = Location.query.filter_by(id=location_id).first()
+
+    if location:
+        location.name = name
+        location.image = image
+        location.latitude = latitude
+        location.longitude = longitude
+        location.type = type
+        db.session.commit()
+        return True
+    return False
